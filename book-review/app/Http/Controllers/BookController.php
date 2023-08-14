22d455d88320eb8c6f8b5a8a270500f53546c3a2
Abhishek Mishra <mishraavi790@gmail.com>
2023-08-14 22:16:33 +0545

    next task

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -7,11 +7,6 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
     public function index(Request $request)
     {
@@ -24,15 +19,13 @@ class BookController extends Controller
         echo var_dump(request()->query());
 
         $books = Book::when($title, function ($query, $title) {
-            // its the local query scope defined in Book model
+
             return $query->title($title);
         });
-        // here we run the query separately and in between those we would like to optionally do something with filtering or at least sort the book by the most recent ones
 
         // match is similar to switch statement,but the diff is it lets you return a value
         $books = match ($filter) {
-            // if filter value is same as key, you can run something like an arrow function or return a value
-            // resulting query will be returned back to books variable and then we can finally run it
+
             'popular_last_month' => $books->popularLastMonth(),
             'popular_last_6month' => $books->popularLast6Month(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
@@ -45,11 +38,7 @@ class BookController extends Controller
         return view('books.index', ['books' => $books]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
@@ -66,46 +55,35 @@ class BookController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    // we want reveiw in a specific order,this can be done by adding a value to this array 
+                    // we are working on a reviews relationship so i can call latest to sort it
+                    'reviews' => fn($query) => $query->latest(),
+                    //this is how you can load some additional relationship and then do something on them
+                ])
+            ]
+        );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
